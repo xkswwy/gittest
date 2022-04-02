409ab9d185f2b2bb5809ac58d0fe879d52cb1202
wwy <wwyatguigu.com>
2022-04-02 23:05:51 +0800

    push commit2

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master");
         System.out.println("hot fix test");
+        System.out.println("push test");
     }
 }
